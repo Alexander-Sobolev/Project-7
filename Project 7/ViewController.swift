845fe3b50a83c8725ff694a24a69db48f0b8c69c
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-06-17 16:08:12 +0300

    Initial Commit

---
+++ Project 7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 7
+//
+//  Created by Alexander Sobolev on 17.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
